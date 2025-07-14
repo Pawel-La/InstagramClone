cd702574ea53ad2b6481f953bbf05f25bf0fc2ea
Paweł <pawellamza@gmail.com>
2025-07-14 16:21:56 +0200

    sidebar wip

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,49 @@
 import ThemedView from "../components/ThemedView";
-import { Text, StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
+import InstagramWrittenLogo from "../assets/images/instagram-written-logo.svg";
 
 export default function Index() {
   return (
-    <ThemedView>
-      <Text style={{ color: "blue", fontSize: 40 }}>
-        Edit app/index.tsx to edit this screen.
-      </Text>
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.sidebar}>
+        <ThemedView style={styles.innerContainer}>
+          <ThemedView style={styles.mainLogo}>
+            <InstagramWrittenLogo width={100} height={30} fill="red" />
+          </ThemedView>
+        </ThemedView>
+      </ThemedView>
+
+      <ThemedView style={styles.scrollScreen}></ThemedView>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  text: {},
+  container: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  sidebar: {
+    width: 244,
+    borderRightColor: "gray",
+    borderRightWidth: 1,
+    paddingBottom: 20,
+    paddingTop: 8,
+    paddingHorizontal: 12,
+  },
+  innerContainer: {
+    flex: 1,
+    backgroundColor: "red",
+    color: "blue",
+  },
+  mainLogo: {
+    paddingTop: 25,
+    paddingBottom: 16,
+    paddingHorizontal: 12,
+    marginBottom: 19,
+    // backgroundColor: "gray",
+  },
+  scrollScreen: {
+    flex: 1,
+  },
 });
