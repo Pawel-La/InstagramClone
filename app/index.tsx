c3436ceb66212ae867dff545e22fa2bd2cc124c7
Paweł <pawellamza@gmail.com>
2025-07-15 11:00:40 +0200

    moved sidebar to separate component

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,11 @@
 import ThemedView from "../components/ThemedView";
 import { StyleSheet } from "react-native";
-import InstagramWrittenLogo from "../assets/images/instagram-written-logo.svg";
+import SideBar from "@/components/SideBar";
 
 export default function Index() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedView style={styles.sidebar}>
-        <ThemedView style={styles.innerContainer}>
-          <ThemedView style={styles.mainLogo}>
-            <InstagramWrittenLogo width={100} height={30} fill="red" />
-          </ThemedView>
-        </ThemedView>
-      </ThemedView>
-
+      <SideBar />
       <ThemedView style={styles.scrollScreen}></ThemedView>
     </ThemedView>
   );
@@ -22,26 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
-  },
-  sidebar: {
-    width: 244,
-    borderRightColor: "gray",
-    borderRightWidth: 1,
-    paddingBottom: 20,
-    paddingTop: 8,
-    paddingHorizontal: 12,
-  },
-  innerContainer: {
-    flex: 1,
-    backgroundColor: "red",
-    color: "blue",
-  },
-  mainLogo: {
-    paddingTop: 25,
-    paddingBottom: 16,
-    paddingHorizontal: 12,
-    marginBottom: 19,
-    // backgroundColor: "gray",
   },
   scrollScreen: {
     flex: 1,
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,12 +1,42 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { StyleSheet } from "react-native";
+import ThemedText from "./ThemedText";
+import ThemedView from "./ThemedView";
+import InstagramWrittenLogo from "../assets/images/instagram-written-logo.svg";
 
 const SideBar = () => {
   return (
-    <View>
-      <Text>SideBar</Text>
-    </View>
+    <ThemedView style={styles.sidebar}>
+      <ThemedView style={styles.innerContainer}>
+        <ThemedView style={styles.mainLogo}>
+          <ThemedText>
+            <InstagramWrittenLogo width={100} height={30} />
+          </ThemedText>
+        </ThemedView>
+      </ThemedView>
+    </ThemedView>
   );
 };
 
 export default SideBar;
+
+const styles = StyleSheet.create({
+  sidebar: {
+    width: 244,
+    borderRightColor: "gray",
+    borderRightWidth: 1,
+    paddingBottom: 20,
+    paddingTop: 8,
+    paddingHorizontal: 12,
+  },
+  innerContainer: {
+    flex: 1,
+    backgroundColor: "red",
+    color: "blue",
+  },
+  mainLogo: {
+    paddingTop: 25,
+    paddingBottom: 16,
+    paddingHorizontal: 12,
+    marginBottom: 19,
+  },
+});
