051643d04ff682f4d400eca47417f9493f5d8133
Paweł <pawellamza@gmail.com>
2025-07-23 15:21:59 +0200

    renaming

--- src/components/ThemedCustomIcon/ThemedCustomIcon.tsx
+++ src/components/ThemedCustomIcon/ThemedCustomIcon.tsx
@@ -1,9 +1,9 @@
 import { SvgProps } from "react-native-svg";
 
-import DirectionLogo from "@/src/assets/images/direction.svg";
-import HeartLogo from "@/src/assets/images/heart.svg";
-import InstagramWrittenLogo from "@/src/assets/images/instagram-written-logo.svg";
-import MoreLogo from "@/src/assets/images/more.svg";
+import DirectionIcon from "@/src/assets/images/direction.svg";
+import HeartIcon from "@/src/assets/images/heart.svg";
+import InstagramWrittenIcon from "@/src/assets/images/instagram-written-logo.svg";
+import MoreIcon from "@/src/assets/images/more.svg";
 import { useThemeColors } from "@/src/hooks/useThemeColors";
 
 type CustomIconNameProps = "direction" | "heart" | "more" | "instagram-written";
@@ -31,7 +31,7 @@ export default function ThemedCustomIcon({
   switch (name) {
     case "direction": {
       return (
-        <DirectionLogo
+        <DirectionIcon
           fill={iconFill}
           color={iconColor}
           width={width}
@@ -42,16 +42,16 @@ export default function ThemedCustomIcon({
     }
     case "heart": {
       return (
-        <HeartLogo fill={iconColor} width={width} height={height} {...props} />
+        <HeartIcon fill={iconColor} width={width} height={height} {...props} />
       );
     }
     case "more": {
       return (
-        <MoreLogo color={iconColor} width={width} height={height} {...props} />
+        <MoreIcon color={iconColor} width={width} height={height} {...props} />
       );
     }
     case "instagram-written": {
-      return <InstagramWrittenLogo color={iconColor} {...props} />;
+      return <InstagramWrittenIcon color={iconColor} {...props} />;
     }
   }
 }
