458650ba027feecca8d8766ba6aeba33fd2e33e5
Paweł <pawellamza@gmail.com>
2025-08-06 14:42:04 +0200

    detail for consistency

--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -1,4 +1,4 @@
-import { BORDER_SIZE, SPACING } from '@/src/utils/theme';
+import { BORDER_SIZE, FONT_WEIGHT, SPACING } from '@/src/utils/theme';
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     marginBottom: SPACING.xl,
   },
   selectedButtonText: {
-    fontWeight: '700',
+    fontWeight: FONT_WEIGHT.bold,
   },
 });
 
