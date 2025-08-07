4304779087fece0669e4c3f8d303a8ec3256efa3
Paweł <pawellamza@gmail.com>
2025-08-07 15:48:01 +0200

    param name change

--- src/context/helpers.ts
+++ src/context/helpers.ts
@@ -1,13 +1,13 @@
 import { Context, useContext } from 'react';
 
 function useContextWrapper<T>(
-  message: string,
+  errorMessage: string,
   context: Context<T | undefined>
 ): T {
   const value = useContext(context);
 
   if (value === undefined) {
-    throw new Error(message);
+    throw new Error(errorMessage);
   }
 
   return value;
