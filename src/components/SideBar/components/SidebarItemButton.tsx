fb3852197849e8be21e06eae04beb7a63efd5b80
Paweł <pawellamza@gmail.com>
2025-08-13 16:49:11 +0200

    small changes

--- src/components/SideBar/components/SidebarItemButton.tsx
+++ src/components/SideBar/components/SidebarItemButton.tsx
@@ -1,7 +1,7 @@
 import { Pressable, StyleSheet, View } from 'react-native';
 
 import ThemedText from '@/src/components/ThemedText';
-import { ThemeProps, useThemeContext } from '@/src/context/ThemeContext';
+import { useThemeContext } from '@/src/context/ThemeContext';
 import useHover from '@/src/hooks/useHover';
 import {
   BORDER_RADIUS,
@@ -25,26 +25,24 @@ export function SidebarItemButton({
 }: SidebarItemButtonProps) {
   const { theme: theme } = useThemeContext();
   const { isHovered, ref } = useHover();
-  const styles = createStyles(theme, isHovered);
 
   return (
-    <Pressable onPress={onPress}>
+    <Pressable onPress={onPress} ref={ref}>
       {({ pressed }) => (
         <View
           style={[
             styles.container,
-            { transform: [{ scale: pressed ? 0.96 : 1 }] },
+            {
+              backgroundColor: isHovered
+                ? theme.background_hovered
+                : theme.background,
+            },
+            pressed && { transform: [{ scale: 0.96 }] },
           ]}
-          ref={ref}
         >
           {isSelected ? sidebarItem.selectedIcon : sidebarItem.icon}
 
-          <ThemedText
-            style={[
-              styles.text,
-              isSelected && { fontWeight: FONT_WEIGHT.bold },
-            ]}
-          >
+          <ThemedText style={[styles.text, isSelected && styles.selectedText]}>
             {sidebarItem.label}
           </ThemedText>
         </View>
@@ -53,21 +51,19 @@ export function SidebarItemButton({
   );
 }
 
-function createStyles(theme: ThemeProps, isHovered: boolean) {
-  const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-      padding: SPACING.md,
-      marginVertical: SPACING.xxs,
-      gap: SPACING.lg,
-      borderRadius: BORDER_RADIUS.md,
-      backgroundColor: isHovered ? theme.background_hovered : theme.background,
-    },
-    text: {
-      fontSize: FONT_SIZE.md,
-      lineHeight: LINE_HEIGHT.lg,
-    },
-  });
-
-  return styles;
-}
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: SPACING.md,
+    marginVertical: SPACING.xxs,
+    gap: SPACING.lg,
+    borderRadius: BORDER_RADIUS.md,
+  },
+  text: {
+    fontSize: FONT_SIZE.md,
+    lineHeight: LINE_HEIGHT.lg,
+  },
+  selectedText: {
+    fontWeight: FONT_WEIGHT.bold,
+  },
+});
