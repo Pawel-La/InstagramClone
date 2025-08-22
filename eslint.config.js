3599242e6ad8a315ae8e548f81194818228381ef
Paweł <pawellamza@gmail.com>
2025-08-22 18:36:34 +0200

    added new prettier + eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,37 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ["dist/*"],
+    ignores: ['dist/*'],
+    rules: {
+      // Sorting within an import
+      'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
+      // Sorting import statements
+      'import/order': [
+        'error',
+        {
+          groups: [['external', 'builtin'], 'internal', ['sibling', 'parent'], 'index'],
+          pathGroups: [
+            {
+              pattern: '@(react|react-native)',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@src/**',
+              group: 'internal',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['internal', 'react'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
   },
 ]);
